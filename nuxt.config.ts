b0099b5371e030d03cb714861a31f1340fe78e15
tamaina <tamaina@hotmail.co.jp>
2022-09-02 00:58:02 +0900

    :v:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  dir: {
+    public: 'content',
+  },
   modules: [
-    '@nuxt/content',
     '@nuxt/image-edge',
+    '@nuxt/content',
   ],
   router: {
     // router config https://nuxtjs.org/docs/configuration-glossary/configuration-router/
