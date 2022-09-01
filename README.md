d942faafd9c5f71a2cc8357f3b5eb2772b81e7c7
tamaina <tamaina@hotmail.co.jp>
2022-09-01 18:52:29 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-jvh8nt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-jvh8nt)
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { data } = await useAsyncData(() => queryContent('/').find())
+</script>
+
+<template>
+  <template v-for="x in data" :key="x">
+  <a :href="x._path">{{ x }}</a><br>
+  </template>
+
+  <ContentDoc path="/" />
+</template>
