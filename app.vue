cc70c4603e854cbf33a0ba674397acc9908bc6a4
tamaina <tamaina@hotmail.co.jp>
2022-09-05 04:39:17 +0900

    v2.2

--- app.vue
+++ app.vue
@@ -1,10 +1,20 @@
 <template>
-  <NuxtLayout :name="(content && content.layout) || 'default'">
+  <NuxtLayout :name="layout">
     <NuxtPage />
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 const route = useRoute();
+const router = useRouter();
 const { data: content } = await useAsyncData(() => queryContent(route.path).findOne());
+const layout = computed(() => (content.value && content.value.layout) || 'default');
+
+watch(router.currentRoute, (newRoute) => {
+  queryContent(newRoute.path).findOne()
+  .then(async newContent => {
+    await nextTick();
+    content.value = newContent;
+  });
+});
 </script>
