ef9bfe1010a5200b7573b0f31aa63be3b4103510
tamaina <tamaina@hotmail.co.jp>
2022-09-05 03:33:06 +0900

    fix app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <NuxtLayout>
+  <NuxtLayout :name="(content && content.layout) || 'default'">
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const route = useRoute();
+const { data: content } = await useAsyncData(() => queryContent(route.path).findOne());
+</script>
