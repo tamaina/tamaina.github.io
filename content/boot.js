4665f1689c5c19a3168f030ccc75b25f7eccb891
tamaina <tamaina@hotmail.co.jp>
2022-09-17 02:24:04 +0900

    :v:

--- content/boot.js
+++ content/boot.js
@@ -1,16 +1,18 @@
 (() => {
-const bootOnReady = () => {
+const setDark = () => {
   if (typeof window === 'undefined') return;
 
   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    console.log('BOOT: SET DARK');
     document.documentElement.classList.add('dark');
   } else {
+    console.log('BOOT: SET LIGHT');
     document.documentElement.classList.remove('dark');
   }
 };
 
-if (document.readyState !== 'loading') window.addEventListener('DOMContentLoaded', bootOnReady);
-else bootOnReady();
+if (!document?.documentElement) window.addEventListener('DOMContentLoaded', setDark);
+else setDark();
 })();
 
 window.adsbygoogle = window.adsbygoogle || [];
