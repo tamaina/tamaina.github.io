98485adfa35a9a5a375235d787c614e488901370
tamaina <tamaina@hotmail.co.jp>
2023-02-17 05:49:50 +0000

    :v:

--- docs/boot.js
+++ docs/boot.js
@@ -1,18 +1,2 @@
-(() => {
-const setDark = () => {
-  if (typeof window === 'undefined') return;
-
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    console.log('BOOT: SET DARK');
-    document.documentElement.classList.add('dark');
-  } else {
-    console.log('BOOT: SET LIGHT');
-    document.documentElement.classList.remove('dark');
-  }
-};
-
-if (!document?.documentElement) window.addEventListener('DOMContentLoaded', setDark);
-else setDark();
-})();
 
 window.adsbygoogle = window.adsbygoogle || [];
