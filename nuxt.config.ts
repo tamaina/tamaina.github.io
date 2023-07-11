cf200fb941d1d12c5711f6432bc1461ea693414a
tamaina <tamaina@hotmail.co.jp>
2023-07-11 04:25:41 +0000

    analytics?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,4 +54,7 @@ export default defineNuxtConfig({
     //provider: 'netlify',
     screens: { xs: 320, sm: 640, md: 768, lg: 1024, xl: 1280, xxl: 1536, '2xl': 1536, max: 8192 },
   },
+  plugins: [
+    { src: '~/plugins/vercel.ts', mode: 'client' },
+  ],
 });
