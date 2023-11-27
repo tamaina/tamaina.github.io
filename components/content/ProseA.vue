66d81964b815137558309cb49eadbcd14c751348
tamaina <tamaina@hotmail.co.jp>
2023-11-27 18:37:55 +0000

    Fix misssing link

--- components/content/ProseA.vue
+++ components/content/ProseA.vue
@@ -10,14 +10,18 @@ const { page } = useContent();
 const target = computed(() => props.blank === true ? '_blank' : props.href.startsWith('http') ? '_blank' : props.blank ? '_blank' : '_self');
 
 const linkTo = computed(() => {
-  if (props.href.startsWith('http') || props.href.startsWith('/') || !page.value) {
+  if (props.href.startsWith('http') || !page.value) {
     return props.href;
+  }
+
+  if (props.href.startsWith('/')) {
+    return props.href.replaceAll(' ', '-').replaceAll('%20', '-');
   }
 
   const indexRegExp = /\/index\.(md|yml|yaml|json)$/;
   const isIndex = indexRegExp.test(page.value._file);
 
-  return isIndex ? `${page.value._path}/${props.href.startsWith('./') ? props.href.slice(2) : props.href}` : props.href;
+  return (isIndex ? `${page.value._path}/${props.href.startsWith('./') ? props.href.slice(2) : props.href}` : props.href).replaceAll(' ', '-').replaceAll('%20', '-');
 });
 </script>
 
