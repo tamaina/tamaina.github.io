59fcd6cd68b8e56ba3d51c7aa309262a2c5db7b4
tamaina <tamaina@hotmail.co.jp>
2023-11-29 16:14:40 +0000

    xperia 5 iv

--- components/content/ProseA.vue
+++ components/content/ProseA.vue
@@ -1,6 +1,8 @@
 // https://github.com/nuxt/content/blob/ec3c61f3ce6dfd79624551bb3ec9bf3e1330ea59/src/runtime/components/Prose/ProseA.vue
 
 <script setup lang="ts">
+import { followNuxtContentUrlConversion } from '~/composables/followNuxtContentUrlConversion';
+
 const props = defineProps<{
   href: string;
   blank?: boolean;
@@ -15,13 +17,13 @@ const linkTo = computed(() => {
   }
 
   if (props.href.startsWith('/')) {
-    return props.href.replaceAll(' ', '-').replaceAll('%20', '-');
+    return followNuxtContentUrlConversion(props.href);
   }
 
   const indexRegExp = /\/index\.(md|yml|yaml|json)$/;
   const isIndex = indexRegExp.test(page.value._file);
 
-  return (isIndex ? `${page.value._path}/${props.href.startsWith('./') ? props.href.slice(2) : props.href}` : props.href).replaceAll(' ', '-').replaceAll('%20', '-');
+  return followNuxtContentUrlConversion(isIndex ? `${page.value._path}/${props.href.startsWith('./') ? props.href.slice(2) : props.href}` : props.href);
 });
 </script>
 
