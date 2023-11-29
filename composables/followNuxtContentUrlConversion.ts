9515ab177fe7befcac912faaa8122cf4d44888da
tamaina <tamaina@hotmail.co.jp>
2023-11-29 16:26:27 +0000

    fix followNuxtContentUrlConversion

--- composables/followNuxtContentUrlConversion.ts
+++ composables/followNuxtContentUrlConversion.ts
@@ -1,7 +1,7 @@
 export const followNuxtContentUrlConversion = (_url: string) => {
     let url = _url;
-    url = url.replace(' ', '-');
-    url = url.replace('%20', '-');
+    url = url.replaceAll(' ', '-');
+    url = url.replaceAll('%20', '-');
     url = url.toLowerCase();
     return url;
 };
