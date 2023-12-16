9066545e010e7b6d6ee9ec171ef1b84bf1195af0
tamaina <tamaina@hotmail.co.jp>
2023-12-16 15:12:48 +0000

    AdInArticle

---
+++ components/AdInArticle.vue
@@ -0,0 +1,19 @@
+<template>
+  <ins
+    class="adsbygoogle"
+    :class="$style.ins"
+    style="display:block; text-align:center;"
+    data-ad-layout="in-article"
+    data-ad-format="fluid"
+    data-ad-client="ca-pub-1736621122676736"
+    :data-ad-slot="isDark() ? 5155586462 : 9613706222"
+    v-push-ad
+  >
+  </ins>
+</template>
+
+<style module>
+.ins {
+  min-height: 11rem;
+}
+</style>
