35ba50ce3f3b00126156a778a57e81e393efc6e6
tamaina <tamaina@hotmail.co.jp>
2023-12-28 17:34:59 +0000

    remove console.log

--- composables/isDark.ts
+++ composables/isDark.ts
@@ -1,5 +1,5 @@
 //export const isDark = () => window.matchMedia('(prefers-color-scheme: dark)').matches;
 export const isDark = () => {
-    console.log('override isDark() to false');
+    // console.log('override isDark() to false');
     return false;
 };
