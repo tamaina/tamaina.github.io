64c04a321e6a0b4dcf17bbd55e0d3061a26846fa
tamaina <tamaina@hotmail.co.jp>
2024-01-19 18:17:50 +0000

    fix mergin

--- components/content/ProseTable.vue
+++ components/content/ProseTable.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="table-responsive">
-    <table class="table table-light border-dark">
+<div class="table-responsive mb-3">
+    <table class="table table-light border-dark mb-0">
         <slot />
     </table>
 </div>
